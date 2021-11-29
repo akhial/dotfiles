cff5a9889b98cdd97cec64d578579919e9e83b68
Adel Khial <adel.khial@gmail.com>
2021-11-29 12:09:05 +0100

    Add rofi

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,29 @@
+-- Adel Khial's XMonad config
+-- 28/11/2021
+
 import XMonad
 import XMonad.Config.Desktop
+import XMonad.Util.EZConfig (additionalKeysP)
 
-main = xmonad desktopConfig
-  { terminal = "alacritty"
-  , modMask = mod4Mask
-  }
+-- Basic configuration
+myTerminal = "alacritty"
+myModMask = mod4Mask -- Super key
+myBorderWidth = 2
+myNormalBorderColor = "#a89984" -- Gray
+myFocusedBorderColor = "#458588" -- Blue
 
+-- Keybindings
+myKeys :: [(String, X ())]
+myKeys =
+    [ ("M-S-<Return>", spawn "rofi -show drun")
+    , ("M-<Return>", spawn (myTerminal))
+    ]
+
+main = xmonad $ desktopConfig
+    { terminal = myTerminal
+    , modMask = myModMask
+    , borderWidth = myBorderWidth
+    , normalBorderColor = myNormalBorderColor
+    , focusedBorderColor = myFocusedBorderColor
+    }
+    `additionalKeysP` myKeys
