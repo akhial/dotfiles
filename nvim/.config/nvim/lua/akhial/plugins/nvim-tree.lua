8e78c22594d5133880e13e5dc9d781e8d443b9c1
Adel Khial <adel.khial@gmail.com>
2024-04-24 11:28:21 +0100

    Add colorizer and config

--- nvim/.config/nvim/lua/akhial/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/akhial/plugins/nvim-tree.lua
@@ -3,6 +3,13 @@ return {
     lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("nvim-tree").setup {}
+        require("nvim-tree").setup {
+            view = {
+                width = 40
+            },
+            renderer = {
+                group_empty = true
+            }
+        }
     end
 }
--- nvim/.config/nvim/lua/akhial/plugins/telescope.lua
+++ nvim/.config/nvim/lua/akhial/plugins/telescope.lua
@@ -3,11 +3,15 @@ return {
     branch = "0.1.x",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+        require("telescope").setup {
+            pickers = {
+                find_files = { prompt_prefix = "🔍 ", hidden = true },
+                git_files = { prompt_prefix = "󰊢 " }
+            }
+        }
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
         vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
-        vim.keymap.set("n", "<leader>fs", function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
     end
 }
