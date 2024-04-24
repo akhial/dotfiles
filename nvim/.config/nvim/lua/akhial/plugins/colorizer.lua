51adec2562b2ec39e334eae70b9b87f88af35e29
Adel Khial <adel.khial@gmail.com>
2024-04-24 12:43:06 +0100

    Use maintained colorizer

--- nvim/.config/nvim/lua/akhial/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/akhial/plugins/colorizer.lua
@@ -1,6 +1,11 @@
 return {
-    "norcalli/nvim-colorizer.lua",
+    "NvChad/nvim-colorizer.lua",
     config = function()
-        require("colorizer").setup()
+        require("colorizer").setup {
+            user_default_options = {
+                names = false,
+                mode = "virtualtext"
+            }
+        }
     end
 }
